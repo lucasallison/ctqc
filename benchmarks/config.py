b0256e5b96439759fc33ea53050a1d377e126169
Lucas Allison <lucasgallison@gmail.com>
2024-10-17 04:15:35 -0400

    Changed config

--- benchmarks/config.py
+++ benchmarks/config.py
@@ -4,7 +4,7 @@ from simulators.qcec import QCEC
 from simulators.ctqc import CTQC
 
 # Timeout in seconds
-TIMEOUT = 30 * 60
+TIMEOUT = 10 * 60
 
 # Path to the directory that will contain the benchmark results
 # RESULTS_DIR = 'reproduced_results'
@@ -21,95 +21,7 @@ EQUIV_BENCHMARKS = [
     'dirs': ('mqt_benchmarks/transp_circuits/transp/', 'mqt_benchmarks/transp_circuits/opt/'),
     'benchmark_name': 'transp_opt',
     'results_subdir': 'mqt_scaled/all', 
-    'equiv_checkers': [CTQC(), QCEC(), QuokkaSharp()]
+    'equiv_checkers': [CTQC(), CTQC(['-d', 'ptrees']), CTQC(['-d', 'rbitvec']), CTQC(['-d', 'cbitvec']), QCEC(), QuokkaSharp()]
     },
-
-    # KetGPT benchmarks
-    # {
-    # 'dirs': ('ketgpt_benchmarks/transp_circuits/transp/', 'ketgpt_benchmarks/transp_circuits/opt/'),
-    # 'benchmark_name': 'transp_opt',
-    # 'results_subdir': 'ketgpt/all', 
-    # 'equiv_checkers': [CTQC(), Qiskit(), QCEC(), QuokkaSharp()]
-    # },
-
-    # # QuokkaSharp benchmarks
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/opt'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_opt', 
-    # 'results_subdir': 'quokka_sharp/all', 
-    # 'equiv_checkers': [CTQC(), Qiskit(), QCEC(), QuokkaSharp()]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/flip'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_flip',
-    # 'results_subdir': 'quokka_sharp/all', 
-    # 'equiv_checkers': [CTQC(), Qiskit(), QCEC(), QuokkaSharp()]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/gm'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_gm',
-    # 'results_subdir': 'quokka_sharp/all',
-    # 'equiv_checkers': [CTQC(), Qiskit(), QCEC(), QuokkaSharp()]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/shift4'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_shift4',
-    # 'results_subdir': 'quokka_sharp/all',
-    # 'equiv_checkers': [CTQC(), Qiskit(), QCEC(), QuokkaSharp()]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/shift7'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_shift7', 
-    # 'results_subdir': 'quokka_sharp/all', 
-    # 'equiv_checkers': [CTQC(), Qiskit(), QCEC(), QuokkaSharp()]
-    # },
-
-    # # QuokkaSharp benchmarks: Generator Sets
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/opt'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_opt', 
-    # 'results_subdir': 'quokka_sharp/gensets', 
-    # 'equiv_checkers': [CTQC(['-d', 'map']), 
-    #                    CTQC(['-d', 'rbitvec']), 
-    #                    CTQC(['-d', 'cbitvec']), 
-    #                    CTQC(['-d', 'ptrees'])]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/flip'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_flip',
-    # 'results_subdir': 'quokka_sharp/gensets', 
-    # 'equiv_checkers': [CTQC(['-d', 'map']), 
-    #                    CTQC(['-d', 'rbitvec']), 
-    #                    CTQC(['-d', 'cbitvec']), 
-    #                    CTQC(['-d', 'ptrees'])]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/gm'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_gm',
-    # 'results_subdir': 'quokka_sharp/gensets', 
-    # 'equiv_checkers': [CTQC(['-d', 'map']), 
-    #                    CTQC(['-d', 'rbitvec']), 
-    #                    CTQC(['-d', 'cbitvec']), 
-    #                    CTQC(['-d', 'ptrees'])]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/shift4'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_shift4',
-    # 'results_subdir': 'quokka_sharp/gensets', 
-    # 'equiv_checkers': [CTQC(['-d', 'map']), 
-    #                    CTQC(['-d', 'rbitvec']), 
-    #                    CTQC(['-d', 'cbitvec']), 
-    #                    CTQC(['-d', 'ptrees'])]
-    # },
-    # {
-    # 'dirs': ('quokka_sharp_benchmarks/algorithm_distilled/origin', 'quokka_sharp_benchmarks/algorithm_distilled/shift7'),
-    # 'benchmark_name': 'qsharp_algorithm_origin_shift7', 
-    # 'results_subdir': 'quokka_sharp/gensets', 
-    # 'equiv_checkers': [CTQC(['-d', 'map']), 
-    #                    CTQC(['-d', 'rbitvec']), 
-    #                    CTQC(['-d', 'cbitvec']), 
-    #                    CTQC(['-d', 'ptrees'])]
-    # },
-
 ]
 
--- benchmarks/mqt_benchmarks/transpile.sh
+++ benchmarks/mqt_benchmarks/transpile.sh
@@ -1,3 +1,23 @@
-rm -r transp_circuits
-python ../transpiler.py -q circuits -o transp_circuits -b h s cx rz -opt 2 -t 300 --qasm2
-fd . transp_circuits -e qasm -I | xargs sed -i '' -E '/(measure|barrier)/d'
+python ../transpiler.py -q dj -o dj_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q ghz -o ghz_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q gs -o gs_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q qft -o qft_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q qnn -o qnn_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q qpe -o qpe_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q realamp -o realamp_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q su -o su_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q twolocal -o twolocal_transp -b h s cx rz -opt 2 -t 1200 --qasm2
+python ../transpiler.py -q wstate -o wstate -b h s cx rz -opt 2 -t 1200 --qasm2
+
+fd . transp_circuits -e qasm -I | xargs sed -i -E '/(measure|barrier)/d'
+
+tar -czvf dj_transp.tar.gz dj_transp
+tar -czvf ghz_transp.tar.gz ghz_transp
+tar -czvf gs_transp.tar.gz gs_transp
+tar -czvf qft_transp.tar.gz qft_transp
+tar -czvf qnn_transp.tar.gz qnn_transp
+tar -czvf qpe_transp.tar.gz qpe_transp
+tar -czvf realamp_transp.tar.gz realamp_transp
+tar -czvf su_transp.tar.gz su_transp
+tar -czvf twolocal_transp.tar.gz twolocal_transp
+tar -czvf wstate_transp.tar.gz wstate_transp
