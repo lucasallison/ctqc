789d63105573684fdcbc1d257b28aabe4c35c0c8
lucasallison <lucasgallison@gmail.com>
2024-10-24 13:10:43 +0200

    Changed config

--- benchmarks/config.py
+++ benchmarks/config.py
@@ -4,7 +4,7 @@ from simulators.qcec import QCEC
 from simulators.ctqc import CTQC
 
 # Timeout in seconds
-TIMEOUT = 10 * 60
+TIMEOUT = 1 * 60
 
 # Path to the directory that will contain the benchmark results
 # RESULTS_DIR = 'reproduced_results'
@@ -13,15 +13,21 @@ RESULTS_DIR = 'mqt_scaled_results'
 # Simulation benchmarks
 SIM_BENCHMARKS = []
 
+
+qiskit_optimized = ["dj", "ghz", "graphstate", "qft", "qftentangled", "qnn", "qpeexact", "qpeinexact", "realamp", "su", "twolocal", "wstate"]
+optimized_dirs = ['opt']
+
 # Equivalence benchmarks
 EQUIV_BENCHMARKS = [
 
     # Scaled MQT benchmarks   
     {
-    'dirs': ('mqt_benchmarks/transp_circuits/transp/', 'mqt_benchmarks/transp_circuits/opt/'),
-    'benchmark_name': 'transp_opt',
-    'results_subdir': 'mqt_scaled/all', 
+    'dirs': (f'mqt_benchmarks/qiskit_optimized/{qo}/transp/', f'mqt_benchmarks/qiskit_optimized/{qo}/{od}'),
+    'benchmark_name': f'transp_{od}',
+    'results_subdir': f'mqt_scaled/{od}/{qo}', 
     'equiv_checkers': [CTQC(), CTQC(['-d', 'ptrees']), CTQC(['-d', 'rbitvec']), CTQC(['-d', 'cbitvec']), QCEC(), QuokkaSharp()]
-    },
+    }
+
+    for od in optimized_dirs for qo in qiskit_optimized 
 ]
 
